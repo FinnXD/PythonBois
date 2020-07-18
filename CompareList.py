a6e70e07df12eb1720018b719c8b04e9e8076549
FinnXD <traffordhall@mail.com>
2020-07-18 14:54:29 +0100

    struggling

---
+++ CompareList.py
@@ -0,0 +1,39 @@
+import random
+
+list1 = []
+list2 = []
+list3 = []
+
+for i in range(20):
+    a = random.randint(0, 20)
+    list1.append(a)
+    a = random.randint(0, 20)
+    list2.append(a)
+
+print(list1)
+print(list2)
+
+for n in range(20):
+    for m in range(20):
+        if list1[n] == list2[m]:
+            b = (list1[n])
+            list3.append(b)
+        
+
+
+
+
+        
+
+print(list3)
+        
+
+
+    
+
+ 
+    
+    
+
+
+
