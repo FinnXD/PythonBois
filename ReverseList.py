c59b993266a0ccfcce9a8ad490c45a231cfbc68e
FinnXD <traffordhall@mail.com>
2020-07-19 01:43:50 +0100

    noice

---
+++ ReverseList.py
@@ -0,0 +1,30 @@
+list1 = [1,3,5,7,9,2,4,6,8]
+
+
+for i in range(9):
+    x = len(list1) - i - 1
+    print(list1[x])        
+    
+#example = "trafford hall"
+#print(example[3])
+
+    
+    
+    
+    
+    
+
+
+    
+    
+    
+    
+
+    
+    
+
+
+
+
+
+
