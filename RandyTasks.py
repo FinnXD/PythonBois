92cf1e3d3e6eec8aa7957bc11b9339159fa37521
FinnXD <traffordhall@mail.com>
2020-07-19 20:23:02 +0100

    not too bad lad

--- RandyTasks.py
+++ RandyTasks.py
@@ -5,8 +5,10 @@ def gimmeSomeSpace():
     print()
 
 
-list1 = ["something", "trafford", "hall", "another", "word", "jeff"]
+list1 = ["something", "trafford", "hall", "another", "word", "something"]
 list2 = [0, 1, 2, 3, 4 ,5, 6, 7, 8, 9]
+list3 = ["something", "trafford", "hall", "hall", "trafford", "something"]
+list4 = [1, 2, 3, 4, 3, 2, 1]
 
 #################################
 
@@ -44,3 +46,33 @@ def chooseThing(theList, p):
     
 print( chooseThing(list1, 3))
 print( chooseThing(list2, 5))
+
+gimmeSomeSpace
+
+####################################
+
+
+def backToFront(theList):
+    length = len( theList) - 1
+    for i in range(length):
+        print(f"this is the {i} time")
+        b = length - i
+        if theList[i] != theList[b]:
+            return False
+    return True
+    
+
+
+
+print (backToFront(list1))
+
+
+print (backToFront(list3))
+        
+        
+        
+
+
+    
+    
+
