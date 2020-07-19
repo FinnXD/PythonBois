84c2cfa49d583d156d6e9ba2974004e40edd5012
FinnXD <traffordhall@mail.com>
2020-07-19 13:14:11 +0100

    Keep gooooooo

--- RandyTasks.py
+++ RandyTasks.py
@@ -1,8 +1,19 @@
 
 
+def gimmeSomeSpace():
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+    print()
+
+
 list1 = ["something", "trafford", "hall", "another", "word", "jeff"]
 list2 = [0, 1, 2, 3, 4 ,5, 6, 7, 8, 9]
 
+#################################
 
 
 def firstThing(theList):
@@ -12,6 +23,7 @@ def firstThing(theList):
 print( firstThing(list1))
 print( firstThing(list2))
 
+gimmeSomeSpace()
 
 ##################################
 
@@ -21,10 +33,19 @@ def lastThing(theList):
     return theList[a - 1]
     
 
-
 print( lastThing(list1))
 print( lastThing(list2))
+
+gimmeSomeSpace()
 
 
 
 ##################################
+
+
+def chooseThing(theList, p):
+    return theList[p]
+
+    
+print( chooseThing(list1, 3))
+print( chooseThing(list2, 5))
